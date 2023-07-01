f51607f25d3736df211b0a5d119ffa42103ff53a
Ritika Guleria <ritikaguleria95@gmail.com>
2023-07-01 21:19:45 +0530

    add DecimalToAnyBase

--- FunctionsAndArrays/FunctionAndArrays.java
+++ FunctionsAndArrays/FunctionAndArrays.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class FunctionAndArrays {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        // int d=sc.nextInt();
+        // int ans=getDigitFrequency(n, d);
+        // System.out.println(ans);
+
         int n=sc.nextInt();
-        int d=sc.nextInt();
-        int ans=getDigitFrequency(n, d);
+        int b=sc.nextInt();
+        int ans=DecimalToAnyBase(n, b);
         System.out.println(ans);
     }
 
@@ -28,5 +33,20 @@ public class FunctionAndArrays {
         }
 
         return count;
+    }
+    //Number system questions
+    //2 ) Decimal to any Base
+    public static int DecimalToAnyBase(int n, int b)
+    {
+        int ans=0;
+        int pow=1;
+        while(n>0)
+        {
+            int rem = n % b;
+            n = n / b;
+            ans += rem * pow;
+            pow = pow * 10;
+        }
+        return ans;
     }
 }
