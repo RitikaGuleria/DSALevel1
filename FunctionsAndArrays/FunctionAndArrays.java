2217b34e432ea6ee0de9262178303bab427c3955
Ritika Guleria <ritikaguleria95@gmail.com>
2023-07-11 15:01:09 +0530

    added subtraction of two array

--- FunctionsAndArrays/FunctionAndArrays.java
+++ FunctionsAndArrays/FunctionAndArrays.java
@@ -36,7 +36,8 @@ public class FunctionAndArrays
         // SpanOfAnArray();
         // findElementInAnArray();
         // BarChart();
-        SumOfTwoArrays();
+        // SumOfTwoArrays();
+        DifferenceOfTwoArrays();
     }
 
     // 1) Digit Frequency
@@ -336,6 +337,72 @@ public class FunctionAndArrays
         for(int val : sum)
         {
             System.out.print(val);
+        }
+    }
+    //Difference b/w two arrays
+    public static void DifferenceOfTwoArrays()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n1");
+        int n1=sc.nextInt();
+        int arr1[] = new int[n1];
+        System.out.println("enter arr1 integers");
+        for(int i=0;i<arr1.length;i++)
+        {
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("enter n2");
+        int n2=sc.nextInt();
+        int arr2[] = new int[n2];
+        System.out.println("enter arr2 integers");
+        for(int i=0;i<arr2.length;i++)
+        {
+            arr2[i]=sc.nextInt();
+        }
+
+        int diff[]= new int[n2];
+        int i=arr1.length-1;
+        int j=arr2.length-1;
+        int k=diff.length-1;
+
+        int c=0; 
+
+        while(k>=0){
+            int d=0;
+            int arr1v= i >=0 ? arr1[i] : 0;
+
+            if(arr2[j]+c >= arr1v )
+            {
+                d= arr2[j] + c - arr1v;
+                c=0;
+            }
+            else{
+                d=arr2[j] + c + 10 - arr1v;
+                c=-1;
+            }
+
+            diff[k] = d;
+            i--;
+            j--;
+            k--;
+        }
+
+        //removing front zeros
+        int idx=0;
+        while(idx<diff.length)
+        {
+            if(diff[idx]==0)
+            {
+                idx++;
+            }
+            else{
+                break;
+            }
+        }
+        while(idx<diff.length)
+        {
+            System.out.print(diff[idx]);
+            idx++;
         }
     }
 }
