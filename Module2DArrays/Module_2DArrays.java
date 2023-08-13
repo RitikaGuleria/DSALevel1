80c73bc3931932674bb6264ac108b1298e174569
Ritika Guleria <ritikaguleria95@gmail.com>
2023-08-13 19:19:58 +0530

    added diagonal traversal

--- Module2DArrays/Module_2DArrays.java
+++ Module2DArrays/Module_2DArrays.java
@@ -19,7 +19,8 @@ public class Module_2DArrays {
         //     System.out.println();
         // }
 
-        RotateBy90Degree();
+        // RotateBy90Degree();
+        DiagonalTraversal();
     }
 
     //Matrix Multiplication
@@ -298,6 +299,30 @@ public class Module_2DArrays {
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
+        }
+    }
+
+    // Diagonal Traversal
+    public static void DiagonalTraversal()
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[][] = new int[n][n];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int g=0;g<arr.length;g++)
+        {
+            for(int i=0,j=g ;j<arr.length;i++,j++)
+            {
+                System.out.println(arr[i][j]);
+            }
         }
     }
 }
